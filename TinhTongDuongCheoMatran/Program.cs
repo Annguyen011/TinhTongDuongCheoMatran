50a241af69bb42514749e4cf8674bf4a6f4baf6e
Anngu <Anngu@MSI>
2023-04-08 17:27:24 +0700

    Add project files.

---
+++ TinhTongDuongCheoMatran/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhTongDuongCheoMatran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[,]
+{
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
+
+            int sum = 0;
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                sum += arr[i, i];
+            }
+
+            Console.WriteLine("Sum of main diagonal elements: " + sum);
+
+        }
+    }
+}
